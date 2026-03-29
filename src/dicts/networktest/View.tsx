d354b3ee6d28995e2d0776f46dae30754dc19faa
Yimin Sun <yimin_sun@yunzhijia.com>
2026-03-29 15:05:36 +0800

    fix: click area

--- src/dicts/networktest/View.tsx
+++ src/dicts/networktest/View.tsx
@@ -4,10 +4,9 @@ import './style.scss'
 
 export const NetworktestView: FC<ViewPorps<any>> = ({ result }) => {
     return (
-        <div className="networktest-Container" 
-             eudic-onlinedict-custom-onclick="onNetworkTestClick">
+        <div className="networktest-Container">
             <div id="networktest-global-q" className="networktest-GlobalQ">global_q: </div>
-            <button className="networktest-Button">CLICK ME!</button>
+            <button className="networktest-Button" eudic-onlinedict-custom-onclick="onNetworkTestClick">CLICK ME!</button>
             <div id="network-test-result" className="networktest-Result">
                 Click the button to see the result...
             </div>
